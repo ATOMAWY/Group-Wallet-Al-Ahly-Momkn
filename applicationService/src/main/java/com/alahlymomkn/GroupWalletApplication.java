52c10f803f4391d0243b12a56b50d0dafe0b1662
Yassin Amr <yassin.amr.6767@gmail.com>
2026-09-02 13:09:19 +0200

    added modules

---
+++ applicationService/src/main/java/com/alahlymomkn/GroupWalletApplication.java
@@ -0,0 +1,13 @@
+package com.alahlymomkn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupWalletApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroupWalletApplication.class, args);
+        System.out.println("✅ Al Ahly Momkn Group Wallet System Started Successfully!");
+    }
+}
